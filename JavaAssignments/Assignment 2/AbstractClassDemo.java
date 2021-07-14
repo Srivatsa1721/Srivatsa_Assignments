be9e3d6f8ff85b8ed0a489273d28e9ec87e23b24
Srivatsa1721 <86977848+Srivatsa1721@users.noreply.github.com>
2021-07-14 18:22:50 +0530

    Add files via upload

---
+++ JavaAssignments/Assignment 2/AbstractClassDemo.java
@@ -0,0 +1,19 @@
+package com.CoreJavaAssignment2;
+
+abstract class chocolates //declaring a class abstract without having an abstract method
+{
+	 void chocolates()
+	 {
+		 
+	 }
+}
+
+public class AbstractClassDemo {
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
